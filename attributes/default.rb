3a2719c4f4d61bc349b0802157e2192da16752f1
Braden Wright <braden.m.wright@gmail.com>
2017-03-16 03:09:30 -0500

    bwright: save point

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+default['bb_chef_server']['org']['id'] = 'bb'
+default['bb_chef_server']['org']['name'] = 'BertaBox'
+
 default['chef-server']['accept_license'] = true
 default['chef-server']['addons'] = %w{ manage reporting }
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,31 @@
 # All rights reserved - Do Not Redistribute
 #
 
+org_id = node['bb_chef_server']['org']['id']
+org_name = node['bb_chef_server']['org']['name']
+
+db_users = {
+  "username": "bwright",
+  "first_name": "Braden",
+  "last_name": "Wright",
+  "email": "braden.wright.it@gmail.com",
+  "password": "p@ssw0rd"
+}
+
 include_recipe 'bb_base'
 include_recipe 'chef-server'
 include_recipe 'chef-server::addons'
 #TODO: setup ssl
 #TODO: setup users
+
+bash 'chef_server_setup_user_bwright' do
+  code "chef-server-ctl user-create #{db_users['username']} #{db_users['first_name']} #{db_users['last_name']}  #{db_users['email']} #{db_users['password']} -f /tmp/#{db_users['username']}.key"
+  not_if { ::File.exist? '/tmp/bwright.key' }
+end
+
+bash "chef_server_setup_org_#{org_id}" do
+  code "chef-server-ctl org-create #{org_id} #{org_name} -a bwright -f /etc/chef/#{org_name}-validator.pem"
+  not_if { ::File.exist? "/etc/chef/#{org_name}-validator.pem" }
+end
+
 #TODO: upload cookbooks, data bags, etc
